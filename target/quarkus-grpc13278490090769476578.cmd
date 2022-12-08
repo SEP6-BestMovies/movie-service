6d60943a5335c3a091360ac8b662d847cd47ab67
Priyanka Shrestha <priyankashrestha938@gmail.com>
2022-12-08 18:34:26 +0100

    test

--- target/quarkus-grpc13278490090769476578.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Users\Priyanka\scoop\apps\graalvm22\current\bin\java.EXE" -cp "C:\Users\Priyanka\.m2\repository\io\quarkus\quarkus-grpc-protoc-plugin\2.14.2.Final\quarkus-grpc-protoc-plugin-2.14.2.Final-shaded.jar" io.quarkus.grpc.protoc.plugin.MutinyGrpcGenerator
