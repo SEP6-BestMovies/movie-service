3d05ac5454d650a577861c17acf46d8ceb984f59
Priyanka Shrestha <priyankashrestha938@gmail.com>
2022-12-09 17:38:36 +0100

    Add api config

--- docker-build-jvm.cmd
+++ docker-build-jvm.cmd
@@ -1,2 +1,2 @@
 ./mvnw clean package -Pjvm
-@REM docker build -f src/main/docker/Dockerfile.native -t com/via/sep6/bestmovies/movieservice:dev .
+@REM docker build -f src/main/docker/Dockerfile.legacy-jar -t com/via/sep6/bestmovies/movieservice:dev .
