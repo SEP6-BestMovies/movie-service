a3a5752adad3ececda526486a624026879c03010
Priyanka Shrestha <priyankashrestha938@gmail.com>
2022-12-09 01:21:51 +0100

    Add annotation

--- src/main/java/com/via/sep6/bestmovies/repository/MovieRepository.java
+++ src/main/java/com/via/sep6/bestmovies/repository/MovieRepository.java
@@ -2,7 +2,9 @@ package com.via.sep6.bestmovies.repository;
 
 import com.via.sep6.bestmovies.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
     Movie getOneByTitle(String title);
 }
